aeeb46c1e0682b57db67f5082671af62f87ee434
Sebuuma Daniel <sedzsoft@gmail.com>
2017-08-04 20:20:32 +0300

    Moves application API URLs into environment varibales.

--- src/utilities/urls.js
+++ src/utilities/urls.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://127.0.0.1:5000";
-export const API_URL = BASE_URL + "/api/v1/";
+export const BASE_URL = process.env.REACT_APP_BASE_URL;
+export const API_URL = process.env.REACT_APP_API_URI;
