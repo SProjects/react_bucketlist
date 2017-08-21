cfe7e5c5b2a44aaa64c2f29a95acab55e1c1783c
Sebuuma Daniel <sedzsoft@gmail.com>
2017-08-21 12:32:55 +0300

    Changes class components into functional components.

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,15 +3,11 @@ import React  from "react";
 import BucketlistsContainer from "../containers/BucketlistsContainer";
 import HeaderContainer from "../containers/HeaderContainer";
 
-class Home extends React.Component {
-	render() {
-		return (
-			<div>
-				<HeaderContainer/>
-				<BucketlistsContainer/>
-			</div>
-		);
-	}
-}
+const Home = () => (
+	<div>
+		<HeaderContainer/>
+		<BucketlistsContainer/>
+	</div>
+);
 
 export default Home;
